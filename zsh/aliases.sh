b44aed924703aabe341c08e28cff575f9fa15ec7
Edward Rees <53308037+erees1@users.noreply.github.com>
2020-09-10 10:18:50 +0100

    Added jupyter lab alias

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -10,6 +10,7 @@ alias spy="tail -f"
 alias zrc="cd ~/git/dotfiles/zsh"
 alias dot="cd ~/git/dotfiles"
 alias mkve="virtualenv -p python3"
+alias jpl="juypter lab"
 
 # -------------------------------------------------------------------
 # general
