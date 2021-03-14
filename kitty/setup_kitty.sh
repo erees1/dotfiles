ce75904e02da9ced960cd601b5374089460e64c0
rmhiecr <rmhiecr@login13.myriad.ucl.ac.uk>
2021-03-14 13:42:42 +0000

    Fixed kitty deployment

--- kitty/setup_kitty.sh
+++ kitty/setup_kitty.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+dir="$HOME/.config/kitty"
+mkdir -p $dir
 
-ln -sf "$HOME/git/dotfiles/kitty/kitty.conf" "$HOME/.config/kitty/kitty.conf"
+ln -sf "$HOME/git/dotfiles/kitty/kitty.conf" "$dir/kitty.conf"
+
+mkdir -p $HOME/.terminfo
 
 # Compile Kitty Terminfo if on UCL
-if [ $LOC == "UCL" ] ; then
-   tic -x -o $HOME/.terminfo $HOME/git/kitty/xterm-kitty
+if [ $LOC == "ucl" ] ; then
+   tic -x -o $HOME/.terminfo $HOME/git/dotfiles/kitty/xterm-kitty
+   echo "Compiled terminfo for kitty"
 fi
