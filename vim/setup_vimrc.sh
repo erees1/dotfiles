6f15bcb21dc8fc8ee96e2b8398b1842160c20833
erees1 <edward.r.rees@zoho.com>
2021-04-14 11:21:33 +0100

    Removed regular vim setup - nvim only now

--- vim/setup_vimrc.sh
+++ vim/setup_vimrc.sh
@@ -1,8 +1,3 @@
-# Regular Vim
-echo "source $HOME/git/dotfiles/vim/vimrc.vim" > $HOME/.vimrc
-ln -sf $HOME/git/dotfiles/vim/plug-config/coc-settings.json $HOME/.vim
-
-
 # Neovim
 if [ ! -d  $HOME/.config/nvim ]; then
     mkdir -p $HOME/.config/nvim;
