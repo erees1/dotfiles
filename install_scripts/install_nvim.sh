b61cdcdc499cb7640b3ac31d8a4febaa49a3c3de
Edward Rees <edwardr@speechmatics.com>
2021-06-12 10:21:39 +0000

    Script to instal nvim app images easily

--- install_scripts/install_nvim.sh
+++ install_scripts/install_nvim.sh
@@ -1,4 +1,17 @@
-#! /usr/bin/bash
-set -euxo pipefail
+#! /bin/bash
+set -euo pipefail
 
+USAGE="install_nvim.sh <url>"
 
+if [ "$#" -lt 1 ]; then
+  echo $USAGE
+  exit 1
+fi
+
+rm -rf ~/.local/nvim
+mkdir -p ~/.local/nvim
+cd ~/.local/nvim
+wget $1
+chmod u+x ./nvim.appimage
+./nvim.appimage --appimage-extract
+ln -sf ~/.local/nvim/squashfs-root/usr/bin/nvim ~/.local/bin/nvim
