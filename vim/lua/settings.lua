438cc917f1202c4e367c4b481f4f6b0c0bae2cc4
Edward Rees <edwardr@speechmatics.com>
2021-06-19 16:06:04 +0100

    Added line numbers in vim

--- vim/lua/settings.lua
+++ vim/lua/settings.lua
@@ -6,7 +6,7 @@ vim.cmd('set expandtab') -- Converts tabs to spaces
 vim.o.clipboard = 'unnamedplus'
 
 vim.o.mouse = 'a' -- Enable mouse in all modes
-
+vim.wo.number = true
 vim.cmd(':hi NoneText guifg=bg')
 
 vim.highlight.on_yank { higroup='IncSearch', timeout=1000 }
