f1cd17c7ddf8f03287a10ecd27ece33101402d18
erees1 <edward.r.rees@zoho.com>
2021-06-23 10:35:21 +0100

    vim/setup_init.sh will remove init.vim if exists

--- vim/setup_init.sh
+++ vim/setup_init.sh
@@ -10,6 +10,11 @@ if [ $LOC == "remote" ] ; then
   echo "require('remote')" >> $HOME/.config/nvim/init.lua
 fi
 
+# Remove init.vim if exists as it will conflict with init.lua
+if [ -f $HOME/.config/nvim/init.vim ]; then
+  rm $HOME/.config/nvim/init.vim
+fi
+
 ln -sf $HOME/git/dotfiles/vim/lua $HOME/.config/nvim
 ln -sf $HOME/git/dotfiles/vim/plug-config/coc-settings.json $HOME/.config/nvim/coc-settings.json
 
