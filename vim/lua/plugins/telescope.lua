e687cedf12c7097c7ed914dc22d98f543a17261e
Edward Rees <edwardr@speechmatics.com>
2021-06-25 15:08:45 +0100

    Telescope ignores resources and htmlcov folders

--- vim/lua/plugins/telescope.lua
+++ vim/lua/plugins/telescope.lua
@@ -1,1 +1,2 @@
+require('telescope').setup{ defaults = { file_ignore_patterns = {"resources", "htmlcov"} } }
 vim.api.nvim_set_keymap('n', '<c-p>', '<cmd>Telescope find_files<CR>', { noremap=true, silent=true })
