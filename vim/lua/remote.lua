46391f8725a4e259713dee5b71aec9d8e68cd597
Edward Rees <edwardr@speechmatics.com>
2021-07-02 11:59:20 +0100

    Added shortcut to copy full file path to osc

--- vim/lua/remote.lua
+++ vim/lua/remote.lua
@@ -1,3 +1,12 @@
  vim.api.nvim_command([[
 autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '' | OSCYankReg + | endif
 ]])
+
+vim.api.nvim_command([[
+function! YankNameToOsc()
+let @+ = expand('%:p')
+OSCYankReg +
+endfunction
+]])
+vim.api.nvim_set_keymap('n', '<leader>cp' , ':call YankNameToOsc()<CR>', {noremap=true, silent=true })
+
