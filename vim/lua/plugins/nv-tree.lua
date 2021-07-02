0a692a8ab017ad6c51b9c7952753ccce32f66dad
Edward Rees <edwardr@speechmatics.com>
2021-07-02 11:57:50 +0100

    Plugin updates

--- vim/lua/plugins/nv-tree.lua
+++ vim/lua/plugins/nv-tree.lua
@@ -2,6 +2,10 @@ vim.api.nvim_set_var('nvim_tree_ignore', {'.git', 'node_modules', '.cache' })
 vim.api.nvim_set_var('nvim_tree_auto_open', 0)
 vim.api.nvim_set_var('nvim_tree_quit_on_open',1)
 vim.api.nvim_set_var('nvim_tree_indent_markers',1)
+vim.api.nvim_set_var('nvim_tree_hide_dotfiles',1)
+vim.api.nvim_set_var('nvim_tree_update_cwd', 1)
+vim.api.nvim_set_var('nvim_tree_follow', 1)
+vim.api.nvim_set_var('nvim_tree_hijack_cursor', 0)
 vim.g.nvim_tree_icons = {
     default = '',
     symlink = '',
--- vim/lua/plugins/telescope.lua
+++ vim/lua/plugins/telescope.lua
@@ -1,2 +1,1 @@
 require('telescope').setup{ defaults = { file_ignore_patterns = {"resources", "htmlcov"} } }
-vim.api.nvim_set_keymap('n', '<c-p>', '<cmd>Telescope find_files<CR>', { noremap=true, silent=true })
