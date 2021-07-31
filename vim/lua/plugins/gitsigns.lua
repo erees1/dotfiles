ca61a391b2a532357adc80230a20319cfe185248
Edward Rees <edwardr@speechmatics.com>
2021-07-31 10:54:47 +0100

    Added shortcut for next hunk

--- vim/lua/plugins/gitsigns.lua
+++ vim/lua/plugins/gitsigns.lua
@@ -21,6 +21,7 @@ require('gitsigns').setup {
     ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
     ['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
     ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+    ['n <leader>hn'] = '<cmd>lua require"gitsigns".next_hunk()<CR>',
     ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
 
     -- Text objects
