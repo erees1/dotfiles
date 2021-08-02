569f05c48362625ed1506d0da3c3b0e2ea9d59d7
Edward Rees <edwardr@speechmatics.com>
2021-08-02 14:40:56 +0100

    Update nerdcommenter shortcuts

--- vim/lua/plugins/nerdcommenter.lua
+++ vim/lua/plugins/nerdcommenter.lua
@@ -1,3 +1,3 @@
 vim.api.nvim_set_var('NERDCreateDefaultMappings', 0)
-vim.api.nvim_set_keymap('n', '<leader>ci', ":call NERDComment('n', 'toggle')<cr>", {noremap=true, silent=true })
-vim.api.nvim_set_keymap('v', '<leader>ci', ":call NERDComment('v', 'toggle')<cr>", {noremap=true, silent=true })
+vim.api.nvim_set_keymap('n', '<leader>ci', ":call nerdcommenter#Comment('n', 'toggle')<cr>", {noremap=true, silent=true })
+vim.api.nvim_set_keymap('v', '<leader>ci', ":call nerdcommenter#Comment('v', 'toggle')<cr>", {noremap=true, silent=true })
