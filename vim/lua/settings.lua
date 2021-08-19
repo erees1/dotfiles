76a6e8c05f8b2a6a202207280147ade9e58e0e24
Edward Rees <edwardr@speechmatics.com>
2021-08-19 11:32:28 +0100

    Don't show intro screen and persist undos

--- vim/lua/settings.lua
+++ vim/lua/settings.lua
@@ -1,6 +1,9 @@
 vim.o.termguicolors = true -- Needed for colors
 vim.cmd('colorscheme onedark')
 
+vim.cmd('set shm+=I') -- don't show intro screen
+vim.cmd('set noshowmode') -- don't show --INSERT--
+
 vim.bo.smartindent = false
 vim.cmd('set expandtab') -- Converts tabs to spaces
 vim.cmd('set ts=4') -- Insert 2 spaces for a tab
@@ -11,6 +14,10 @@ vim.cmd('set cursorline')
 vim.o.mouse = 'a' -- Enable mouse in all modes
 vim.wo.number = true
 vim.cmd(':hi NoneText guifg=bg')
+
+--Persistent history
+vim.cmd('set undodir=/tmp/.vim-undo-dir')
+vim.cmd('set undofile')
 
 vim.api.nvim_command([[
 au TextYankPost * silent! lua vim.highlight.on_yank { higroup='IncSearch', timeout=1000 }
