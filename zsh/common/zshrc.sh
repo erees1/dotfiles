199e9e667c299104d35964a769243053decbe7e5
Edward Rees <edwardr@speechmatics.com>
2021-08-19 11:28:38 +0100

    Better vi-mode

--- zsh/common/zshrc.sh
+++ zsh/common/zshrc.sh
@@ -2,7 +2,8 @@ source $HOME/git/dotfiles/vars.sh
 ZSH_DISABLE_COMPFIX=true
 ZSH_THEME="powerlevel10k/powerlevel10k"
 
-plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-completions history-substring-search vi-mode)
+plugins=(zsh-autosuggestions zsh-syntax-highlighting zsh-completions history-substring-search)
+#KEYTIMEOUT=20
 
 # add ./local/bin to path
 p="${HOME}/.local/bin"
@@ -13,8 +14,11 @@ fi
 # Source fzf file
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
+VI_MODE_SET_CURSOR=true
+
 source $ZSH/oh-my-zsh.sh
 source ~/git/dotfiles/zsh/common/p10k.zsh
 source ~/git/dotfiles/zsh/common/aliases.sh
 source ~/git/dotfiles/zsh/common/extras.sh
 source ~/git/dotfiles/zsh/common/keybindings.sh
+source ~/git/dotfiles/zsh/common/vi-mode.sh
