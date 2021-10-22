61736d66f13a8664e949f52875115eefbd746e26
Edward Rees <edwardr@speechmatics.com>
2021-10-22 17:50:29 +0100

    Nvim tree opens at file

--- vim/lua/plugins/nv-tree.lua
+++ vim/lua/plugins/nv-tree.lua
@@ -1,4 +1,3 @@
-require'nvim-tree'.setup()
 vim.api.nvim_set_var('nvim_tree_ignore', {'.git', 'node_modules', '.cache' })
 vim.api.nvim_set_var('nvim_tree_auto_open', 0)
 vim.api.nvim_set_var('nvim_tree_quit_on_open',0)
@@ -15,3 +14,15 @@ vim.g.nvim_tree_icons = {
 }
 
 vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap=true, silent=true })
+require'nvim-tree'.setup {
+  update_focused_file = {
+    -- enables the feature
+    enable      = true,
+    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+    -- only relevant when `update_focused_file.enable` is true
+    update_cwd  = false,
+    -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+    -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+    ignore_list = {}
+  },
+}
