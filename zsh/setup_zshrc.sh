6679011747a773b9adfef23275f5869f33f616c8
Edward Rees <edwardr@speechmatics.com>
2021-11-15 10:58:45 +0000

    Change order of sourcing so that LS_COLORS get picked up

--- zsh/setup_zshrc.sh
+++ zsh/setup_zshrc.sh
@@ -3,8 +3,12 @@
 DOT_FILES=$HOME/git/dotfiles
 
 echo "deploying on $LOC machine..."
-echo "source $DOT_FILES/zsh/common/zshrc.sh" > $HOME/.zshrc
-echo "source $DOT_FILES/zsh/$LOC/zshrc.sh" >> $HOME/.zshrc
+echo "source $DOT_FILES/zsh/$LOC/zshrc.sh" > $HOME/.zshrc
+echo "source $DOT_FILES/zsh/common/zshrc.sh" >> $HOME/.zshrc
+
+if [ $LOC == 'remote' ]; then
+    echo remote
+fi
 
 # Relaunch zsh
 # zsh
