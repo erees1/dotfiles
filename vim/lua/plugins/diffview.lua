f065536b2d26dc1b73463d3a50f8822d99e719f5
Edward Rees <edwardr@speechmatics.com>
2021-11-15 10:57:25 +0000

    Turn on enhanced diff highlights

--- vim/lua/plugins/diffview.lua
+++ vim/lua/plugins/diffview.lua
@@ -3,7 +3,7 @@ local cb = require'diffview.config'.diffview_callback
 
 require'diffview'.setup {
   diff_binaries = false,    -- Show diffs for binaries
-  enhanced_diff_hl = false, -- See ':h diffview-config-enhanced_diff_hl'
+  enhanced_diff_hl = true, -- See ':h diffview-config-enhanced_diff_hl'
   use_icons = true,         -- Requires nvim-web-devicons
   icons = {                 -- Only applies when use_icons is true.
     folder_closed = "",
