e8e921d006892b9370206d52f5527c2e339b088a
Edward Rees <edwardr@speechmatics.com>
2021-11-16 21:52:41 +0000

    Change require order

--- vim/lua/init.lua
+++ vim/lua/init.lua
@@ -3,9 +3,9 @@ vim.api.nvim_set_keymap('n', '<Space>', '<NOP>', {noremap = true, silent = true}
 vim.g.mapleader = ' '
 
 if require('funcs').is_not_vscode then
+    require('statusline')
     require('plugins')
     require('settings')
     require('keybindings')
-    require('statusline')
 end
 
