ea52454f2899764a5aa7f765bfaaa2a4635ca736
Edward Rees <edwardr@speechmatics.com>
2021-11-16 21:55:18 +0000

    call settings with require

--- vim/lua/plugins/nv-tree.lua
+++ vim/lua/plugins/nv-tree.lua
@@ -17,10 +17,10 @@ require'nvim-tree'.setup {
   },
   update_focused_file = {
     -- enables the feature
-    enable      = true,
+    enable = true,
     -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
     -- only relevant when `update_focused_file.enable` is true
-    update_cwd  = false,
+    update_cwd  = true,
     -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
     -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
     ignore_list = {}
@@ -28,7 +28,7 @@ require'nvim-tree'.setup {
 
   view = {
     -- width of the window, can be either a number (columns) or a string in `%`, for left or right side placement
-    width = tree_width,
+    width = require('settings').tree_width,
   }
 }
 
@@ -37,7 +37,7 @@ function _tree_toggle()
     if require'nvim-tree.view'.win_open() then
         require'bufferline.state'.set_offset(0)
     else
-        require'bufferline.state'.set_offset(tree_width + 1, 'FileTree')
+        require'bufferline.state'.set_offset(require('settings').tree_width + 1, 'FileTree')
     end
     require'nvim-tree'.toggle()
 end
