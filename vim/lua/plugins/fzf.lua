6a9323e46c3722d4081706d19875dff149790606
edwardr <edwardr@speechmatics.com>
2021-11-29 12:16:47 +0000

    deleted unused fzf.lua

--- vim/lua/plugins/fzf.lua
+++
@@ -1,7 +0,0 @@
- vim.api.nvim_command([[
-let g:fzf_colors = {'bg+': ['bg','visual']}
-]])
-
-
---FZF - overwrite telescope
-vim.api.nvim_set_keymap('n', '<c-p>', ":FZF<CR>", { noremap=true, silent=true })
