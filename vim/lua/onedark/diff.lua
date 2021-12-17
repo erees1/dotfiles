a412ff911e533d09ed77e17165ae7367267e9222
edwardr <edwardr@speechmatics.com>
2021-12-17 15:04:32 +0000

    remove underline from diff

--- vim/lua/onedark/diff.lua
+++ vim/lua/onedark/diff.lua
@@ -2,9 +2,9 @@ local M = {}
 function M.init (C)
     local diff = {
       DiffAdd = { fg = C.none, bg = C.diff_add },
-      DiffDelete = { fg = C.none, bg = C.diff_delete },
-      DiffChange = { fg = C.none, bg = C.diff_change },
-      DiffText = { fg = C.none, bg = C.diff_text },
+      DiffDelete = { fg = C.none, bg = C.diff_delete, style = "NONE" },
+      DiffChange = { fg = C.none, bg = C.diff_change, style = "NONE" },
+      DiffText = { fg = C.none, bg = C.diff_text, style = "NONE"  },
       DiffAdded = { fg = C.green },
       DiffRemoved = { fg = C.red },
       DiffFile = { fg = C.cyan },
