e51b56c14844b0c54b95146a090704f881cd270a
edwardr <edwardr@speechmatics.com>
2021-12-17 15:12:15 +0000

    better msg

--- custom_bins/rl.sh
+++ custom_bins/rl.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # readlink -f but copy the result to the clipboard
 path=$(readlink -f $@)
-echo $path
 if tmux info &> /dev/null; then 
     # yk only works in tmux atm
     echo $path | yk
+    echo $path copied to clipboard
+else
+    echo $path
 fi
