0086001aca2d29b2ff907b2e286c9eff96d01e5a
edwardr <edwardr@speechmatics.com>
2021-12-17 15:10:04 +0000

    exit yank.sh if run outside of tmux

--- custom_bins/rl.sh
+++ custom_bins/rl.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
 # readlink -f but copy the result to the clipboard
 path=$(readlink -f $@)
 echo $path
-echo $path | yk
+if tmux info &> /dev/null; then 
+    # yk only works in tmux atm
+    echo $path | yk
+fi
--- custom_bins/yank.sh
+++ custom_bins/yank.sh
@@ -6,12 +6,14 @@ is_app_installed() {
   type "$1" &>/dev/null
 }
 
+if ! tmux info &> /dev/null; then 
+    echo 'yank.sh only works in tmux atm'
+    exit 1
+fi
 # get data either form stdin or from file
 buf=$(cat "$@")
-if tmux info &> /dev/null; then 
-    copy_backend_remote_tunnel_port=$(tmux show-option -gvq "@copy_backend_remote_tunnel_port")
-    copy_use_osc52_fallback=$(tmux show-option -gvq "@copy_use_osc52_fallback")
-fi
+copy_backend_remote_tunnel_port=$(tmux show-option -gvq "@copy_backend_remote_tunnel_port")
+copy_use_osc52_fallback=$(tmux show-option -gvq "@copy_use_osc52_fallback")
 
 # Resolve copy backend: pbcopy (OSX), reattach-to-user-namespace (OSX), xclip/xsel (Linux)
 copy_backend=""
