eac3518394472039d1da6f178b75a3d87d16cfab
erees1 <edward.r.rees@zoho.com>
2022-01-19 13:31:55 +0000

    commented out potentially redudant opts

--- zsh/common/extras.sh
+++ zsh/common/extras.sh
@@ -2,27 +2,27 @@
 # zsh extra settings
 #-------------------------------------------------------------
 
-setopt RM_STAR_WAIT
-setopt NO_BEEP
-setopt HIST_REDUCE_BLANKS
-setopt HIST_IGNORE_SPACE
-setopt HIST_NO_STORE
-setopt EXTENDED_HISTORY
-setopt HIST_SAVE_NO_DUPS
-setopt HIST_EXPIRE_DUPS_FIRST
-setopt HIST_FIND_NO_DUPS
-setopt completealiases
-setopt always_to_end
-setopt list_ambiguous
-export HISTSIZE=100000 # big big history
-export HISTFILESIZE=100000 # big big history
-unsetopt hup
-unsetopt list_beep
-skip_global_compinit=1
-zstyle ':completion:*' hosts off
+#setopt RM_STAR_WAIT
+#setopt NO_BEEP
+#setopt HIST_REDUCE_BLANKS
+#setopt HIST_IGNORE_SPACE
+#setopt HIST_NO_STORE
+#setopt EXTENDED_HISTORY
+#setopt HIST_SAVE_NO_DUPS
+#setopt HIST_EXPIRE_DUPS_FIRST
+#setopt HIST_FIND_NO_DUPS
+#setopt completealiases
+#setopt always_to_end
+#setopt list_ambiguous
+#export HISTSIZE=100000 # big big history
+#export HISTFILESIZE=100000 # big big history
+#unsetopt hup
+#unsetopt list_beep
+#skip_global_compinit=1
+#zstyle ':completion:*' hosts off
  
-bindkey "^[[A" history-beginning-search-backward
-bindkey "^[[B" history-beginning-search-forward
+#bindkey "^[[A" history-beginning-search-backward
+#bindkey "^[[B" history-beginning-search-forward
  
 
 export FZF_DEFAULT_OPTS='--color=16,bg:-1,bg+:15,hl:4,hl+:4,fg:-1,fg+:-1,gutter:-1,pointer:-1,marker:-1,prompt:1 --height 40% --reverse'
