0950d648c0e906d2f57ca1fe0647ce588812a7cd
erees1 <edward.r.rees@zoho.com>
2022-01-19 13:32:36 +0000

    change comment shortcut

--- vim/lua/plugins/nerdcommenter.lua
+++ vim/lua/plugins/nerdcommenter.lua
@@ -1,3 +1,5 @@
 vim.api.nvim_set_var('NERDCreateDefaultMappings', 0)
-vim.api.nvim_set_keymap('n', '<leader>ci', ":call nerdcommenter#Comment('n', 'toggle')<cr>", {noremap=true, silent=true })
-vim.api.nvim_set_keymap('v', '<leader>ci', ":call nerdcommenter#Comment('v', 'toggle')<cr>", {noremap=true, silent=true })
+vim.api.nvim_set_keymap('n', '<leader>gc', ":call nerdcommenter#Comment('n', 'toggle')<cr>", {noremap=true, silent=true })
+vim.api.nvim_set_keymap('v', '<leader>gc', ":call nerdcommenter#Comment('v', 'toggle')<cr>", {noremap=true, silent=true })
+vim.api.nvim_set_keymap('n', '<C-/>', ":call nerdcommenter#Comment('n', 'toggle')<cr>", {noremap=true, silent=true })
+vim.api.nvim_set_keymap('v', '<C-/>', ":call nerdcommenter#Comment('v', 'toggle')<cr>", {noremap=true, silent=true })
