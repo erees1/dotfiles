061090f1f42981d18683d8d6153413a33e879c64
edwardr <edwardr@speechmatics.com>
2022-01-25 15:35:07 +0000

    update jonah to map in home

--- zsh/remote/aliases.sh
+++ zsh/remote/aliases.sh
@@ -46,7 +46,7 @@ alias msad3="/home/$(whoami)/git/aladdin3/env/singularity.sh -c "$SHELL""
 alias jpl="jupyter lab --no-browser --ip $HOST_IP_ADDR"
 alias ls='ls -hF --color' # add colors for filetype recognition
 alias nv='nvidia-smi'
-function jonah() { docker run --entrypoint bash -it $@ ;} # Enter docker
+function jonah() { docker run -v $HOME:$HOME --entrypoint bash -it $@ ;} # Enter docker
 
 # make file
 alias m='make'
