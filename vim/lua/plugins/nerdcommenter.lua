ed9365b658fd96d3b4d65b0b08f6ee78034f553c
erees1 <edward.r.rees@zoho.com>
2022-01-25 15:57:03 +0000

    update commenter

--- vim/lua/plugins/nerdcommenter.lua
+++ vim/lua/plugins/nerdcommenter.lua
@@ -1,4 +1,5 @@
 vim.api.nvim_set_var('NERDCreateDefaultMappings', 0)
+vim.api.nvim_set_var('NERDSpaceDelims', 0)
 vim.api.nvim_set_keymap('n', '<leader>gc', ":call nerdcommenter#Comment('n', 'toggle')<cr>", {noremap=true, silent=true })
 vim.api.nvim_set_keymap('v', '<leader>gc', ":call nerdcommenter#Comment('v', 'toggle')<cr>", {noremap=true, silent=true })
 vim.api.nvim_set_keymap('n', '<C-/>', ":call nerdcommenter#Comment('n', 'toggle')<cr>", {noremap=true, silent=true })
