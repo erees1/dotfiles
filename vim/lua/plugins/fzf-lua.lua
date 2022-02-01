2f4b04fbdf03a33be88ccfb713e7c26afed02594
erees1 <edward.r.rees@zoho.com>
2022-02-01 19:37:51 +0000

    update fzf to not use printf for compatibility with POSIX find

--- vim/lua/plugins/fzf-lua.lua
+++ vim/lua/plugins/fzf-lua.lua
@@ -4,12 +4,12 @@ vim.api.nvim_set_keymap('n', '<leader>tg', ":FzfLua live_grep<CR>", { noremap=tr
 vim.api.nvim_set_keymap('n', '<leader>tt', ":FzfLua git_files<CR>", { noremap=true, silent=true })
 
 require'fzf-lua'.setup {
-    fzf_layout          = 'reverse',
+    fzf_layout       = 'reverse',
     winopts = {
-    win_height       = 0.80,            -- window height
+    win_height       = 0.80,           -- window height
     win_border       = true,           -- window border? or borderchars?
   },
     files = {
-        find_opts = [[-type f -not -path '*/\.git/*' -not -path '*/resources/*' -not -path '*/testsets/*' -printf '%P\n']],
+        find_opts = [[-type f -not -path '*/\.git/*' -not -path '*/resources/*' -not -path '*/testsets/*']],
     },
 }
