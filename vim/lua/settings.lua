7f0a8ece309150367e0966d8402494adad47b592
erees1 <edward.r.rees@zoho.com>
2022-02-01 19:31:58 +0000

    remove smartindent option

--- vim/lua/settings.lua
+++ vim/lua/settings.lua
@@ -13,7 +13,6 @@ vim.cmd('colorscheme onedark')
 vim.cmd('set shm+=I') -- don't show intro screen
 vim.cmd('set noshowmode') -- don't show --INSERT--
 
-vim.bo.smartindent = false
 vim.cmd('set expandtab') -- Converts tabs to spaces
 vim.cmd('set ts=4') -- Insert 2 spaces for a tab
 vim.cmd('set sw=4') -- Change the number of space characters inserted for indentation
