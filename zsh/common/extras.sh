56d4f927312c13ee4d4553d3f2af1e55f31f1baf
edwardr <edwardr@speechmatics.com>
2022-02-21 10:17:37 +0000

    tweak extra zsh options

--- zsh/common/extras.sh
+++ zsh/common/extras.sh
@@ -2,31 +2,25 @@
 # zsh extra settings
 #-------------------------------------------------------------
 
-#setopt RM_STAR_WAIT
-setopt NO_BEEP
-#setopt HIST_REDUCE_BLANKS
-#setopt HIST_IGNORE_SPACE
-#setopt HIST_NO_STORE
-#setopt EXTENDED_HISTORY
-#setopt HIST_SAVE_NO_DUPS
-#setopt HIST_EXPIRE_DUPS_FIRST
-#setopt HIST_FIND_NO_DUPS
-#setopt completealiases
-#setopt always_to_end
-#setopt list_ambiguous
-#export HISTSIZE=100000 # big big history
-#export HISTFILESIZE=100000 # big big history
-#unsetopt hup
-#unsetopt list_beep
-#skip_global_compinit=1
-#zstyle ':completion:*' hosts off
+setopt RM_STAR_WAIT  # Wait 10 seconds when doing rm *
+setopt NO_BEEP       # No bell sound
+skip_global_compinit=1   # Speeds up start time
+setopt HIST_REDUCE_BLANKS  # Remove blank lines from history
+setopt HIST_IGNORE_SPACE
+setopt HIST_NO_STORE
+setopt EXTENDED_HISTORY
+setopt HIST_SAVE_NO_DUPS
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_FIND_NO_DUPS
+setopt always_to_end
+setopt list_ambiguous
+export HISTSIZE=100000 # big big history
+export HISTFILESIZE=100000 # big big history
+zstyle ':completion:*' hosts off  # Don't autocomple ssh host names
  
-#bindkey "^[[A" history-beginning-search-backward
-#bindkey "^[[B" history-beginning-search-forward
- 
-
+# FZF options - might also affect fzf-lua extenstion in vim
 export FZF_DEFAULT_OPTS='--color=16,bg:-1,bg+:15,hl:4,hl+:4,fg:-1,fg+:-1,gutter:-1,pointer:-1,marker:-1,prompt:1 --height 40% --reverse --color border:46 --border=sharp --prompt="➤  " --pointer="➤ " --marker="➤ "'
-#export FZF_DEFAULT_OPTS='--color fg:-1,bg:-1,hl:46,fg+:40,bg+:233,hl+:46 --color prompt:166,border:46 --height 40%  --border=sharp --prompt="➤  " --pointer="➤ " --marker="➤ " --reverse'
+
 # This speeds up pasting w/ autosuggest
 # https://github.com/zsh-users/zsh-autosuggestions/issues/238
 pasteinit() {
