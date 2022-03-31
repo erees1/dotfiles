0e76af606e8890f7cfcc8dc958f9942fe3bba8d7
erees1 <edward.r.rees@zoho.com>
2022-03-31 10:30:05 +0100

    cleaning

--- vim/lua/keybindings.lua
+++ vim/lua/keybindings.lua
@@ -1,6 +1,5 @@
 -- Key Mappings
--- Note this is not all key mappings, generally ones specific to a plugin 
--- in the plug-config folder
+-- Note this is not all key mappings, generally ones specific to a plugin are in the plug-config folder
 
 -- <leader>s to save
 vim.api.nvim_set_keymap('n', '<leader>s', ':update<CR>', {noremap=true, silent=true }) -- leader s to save
@@ -8,10 +7,6 @@ vim.api.nvim_set_keymap('n', '<leader>s', ':update<CR>', {noremap=true, silent=t
 -- Always use g mode which moves through wrapped lines as if they were actual lines
 vim.api.nvim_set_keymap('n', 'j', 'gj', {noremap=false, silent=true })
 vim.api.nvim_set_keymap('n', 'k', 'gk', {noremap=false, silent=true })
-
--- jk or kj in to escape insert mode
-vim.api.nvim_set_keymap('i', 'jk', '<esc>', {noremap=true, silent=true })
-vim.api.nvim_set_keymap('i', 'kj', '<esc>', {noremap=true, silent=true })
 
 -- Shift + HL to move to start and end of visual line
 vim.api.nvim_set_keymap('n', 'H', 'g^', {noremap=true, silent=true })
@@ -24,21 +19,6 @@ vim.api.nvim_set_keymap('n', 'J', 'L', {noremap=true, silent=true })
 vim.api.nvim_set_keymap('n', 'K', 'H', {noremap=true, silent=true })
 vim.api.nvim_set_keymap('v', 'J', 'L', {noremap=true, silent=true })
 vim.api.nvim_set_keymap('v', 'K', 'H', {noremap=true, silent=true })
-
--- Move lines up and down with the alt key a-la vscode
-vim.api.nvim_set_keymap('n', '<A-j>' , ':m .+1<CR>==', {noremap=true, silent=true })
-vim.api.nvim_set_keymap('n', '<A-k>' , ':m .-2<CR>==', {noremap=true, silent=true })
-vim.api.nvim_set_keymap('i', '<A-j>' , ':m .+1<CR>==gi', {noremap=true, silent=true })
-vim.api.nvim_set_keymap('i', '<A-k>' , ':m .-2<CR>==gi', {noremap=true, silent=true })
-vim.api.nvim_set_keymap('v', '<A-j>' , ":m '>+1<CR>gv=gv", {noremap=true, silent=true })
-vim.api.nvim_set_keymap('v', '<A-k>' , ":m '<-2<CR>gv=gv", {noremap=true, silent=true })
-vim.api.nvim_set_keymap('n', '<A-Down>', ':m .+1<CR>==', {noremap=true, silent=true })
-vim.api.nvim_set_keymap('n', '<A-Up>' , ':m .-2<CR>==', {noremap=true, silent=true })
-vim.api.nvim_set_keymap('i', '<A-Down>', ':m .+1<CR>==gi', {noremap=true, silent=true })
-vim.api.nvim_set_keymap('i', '<A-Up>' , ':m .-2<CR>==gi', {noremap=true, silent=true })
-vim.api.nvim_set_keymap('v', '<A-Down>', ":m '>+1<CR>gv=gv", {noremap=true, silent=true })
-vim.api.nvim_set_keymap('v', '<A-Up>' , ":m '<-2<CR>gv=gv", {noremap=true, silent=true })
-
 -- Copy paste from system buffers to make copy paste behaviour more sane
 vim.api.nvim_set_keymap('v', 'y', '"+y', {noremap=true, silent=true })
 vim.api.nvim_set_keymap('n', 'y', '"+y', {noremap=true, silent=true })
