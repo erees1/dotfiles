7406518ffbfa7b66d66ea3c916efa58f3484b216
edwardr <edwardr@speechmatics.com>
2022-04-01 13:30:23 +0100

    added indent and search options

--- vim/lua/settings.lua
+++ vim/lua/settings.lua
@@ -1,6 +1,7 @@
 -- Config
 local M = {}
 
+-- Config options
 M.tree_width = 35
 M.fzf_on_startup = false
 M.open_tree_on_startup = false
@@ -12,10 +13,11 @@ vim.cmd('colorscheme onedark')
 vim.cmd('set shm+=I') -- don't show intro screen
 vim.cmd('set noshowmode') -- don't show --INSERT--
 
-
 vim.cmd('set expandtab') -- Converts tabs to spaces
 vim.cmd('set ts=4') -- Insert 2 spaces for a tab
 vim.cmd('set sw=4') -- Change the number of space characters inserted for indentation
+vim.cmd('set smartindent')
+vim.cmd('set autoindent')
 
 vim.cmd('set cursorline')
 vim.o.mouse = 'a' -- Enable mouse in all modes
@@ -24,6 +26,10 @@ vim.wo.relativenumber = true
 vim.cmd(':hi NoneText guifg=bg')
 
 vim.cmd('set ttimeoutlen=0')
+
+-- When searching ignore case of search term unless it has caps
+vim.cmd('set smartcase')
+vim.cmd('set ignorecase')
 
 --Persistent history
 vim.cmd('set undodir=/tmp/.vim-undo-dir')
