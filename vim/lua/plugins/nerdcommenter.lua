c2297c0a70a4ee9fcad08c12a8c7c466cfdda017
edwardr <edwardr@speechmatics.com>
2022-04-04 11:34:57 +0100

    added motion like nerdcommenter commands

--- vim/lua/plugins/nerdcommenter.lua
+++ vim/lua/plugins/nerdcommenter.lua
@@ -3,3 +3,7 @@ vim.api.nvim_set_var('NERDDefaultAlign', 'left')
 
 vim.api.nvim_set_keymap('n', '<leader>c', ":call nerdcommenter#Comment('n', 'toggle')<cr>", {noremap=true, silent=true })
 vim.api.nvim_set_keymap('v', '<leader>c', ":call nerdcommenter#Comment('v', 'toggle')<cr>", {noremap=true, silent=true })
+vim.api.nvim_set_keymap('n', '<leader>cip' "Vip:call nerdcommenter#Comment('v', 'toggle')<cr>", {noremap=true, silent=true })
+vim.api.nvim_set_keymap('n', '<leader>cap' "Vap:call nerdcommenter#Comment('v', 'toggle')<cr>", {noremap=true, silent=true })
+vim.api.nvim_set_keymap('n', '<leader>c}' "V}:call nerdcommenter#Comment('v', 'toggle')<cr>", {noremap=true, silent=true })
+vim.api.nvim_set_keymap('n', '<leader>c{' "V{:call nerdcommenter#Comment('v', 'toggle')<cr>", {noremap=true, silent=true })
