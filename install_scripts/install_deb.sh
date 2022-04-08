2f03b22b089aa8d5bf1b5f60e107713f3b3e90b0
edwardr <edwardr@speechmatics.com>
2022-04-08 17:38:59 +0100

    added install_fd.sh and factor out deb installation to install_deb.sh

---
+++ install_scripts/install_deb.sh
@@ -0,0 +1,16 @@
+#! /bin/bash -eu
+set -euo pipefail
+# Helper script to download and install .debs in .local/bin
+
+url=$1
+name=$2
+
+tmp_dir="$HOME/.local/tmp-deb-download" 
+mkdir -p $tmp_dir
+pushd $tmp_dir
+wget $url
+file=$(find ./ -name '*.deb')
+echo $file
+dpkg -x $file $HOME/.local/$name
+rm -rf $tmp_dir
+ln -sf $(readlink -f $HOME/.local/$name/usr/bin/$name) $HOME/.local/bin/$name
--- install_scripts/install_delta.sh
+++ install_scripts/install_delta.sh
@@ -2,7 +2,6 @@
 set -euo pipefail
 
 rm -rf $HOME/.local/delta $HOME/.local/bin/delta
-wget https://github.com/dandavison/delta/releases/download/0.4.4/git-delta_0.4.4_amd64.deb
-dpkg -x git-delta_0.4.4_amd64.deb $HOME/.local/delta
-rm git-delta_0.4.4_amd64.deb
-ln -s $(readlink -f $HOME/.local/delta/usr/bin/delta) $HOME/.local/bin/delta
+url="https://github.com/dandavison/delta/releases/download/0.4.4/git-delta_0.4.4_amd64.deb"
+
+$DOT_DIR/install_scripts/install_deb.sh "$url" "delta"
