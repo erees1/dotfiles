6dbf8ec0abe3c8b310e9d138e3c3c3595f2c59fc
edwardr <edwardr@speechmatics.com>
2022-04-08 18:05:36 +0100

    added exps to tsesh options and bindings

--- custom_bins/tmux-sesh.sh
+++ custom_bins/tmux-sesh.sh
@@ -5,11 +5,18 @@ possible_options="$HOME/git"
 if [ -d "/perish_aml02" ]; then
     possible_options+=" /perish_aml02/edwardr/git"
 fi
+exact=" /exp/$(whoami)"
 
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find $possible_options -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$({ echo $exact; find $possible_options -mindepth 1 -maxdepth 1 -type d; } | fzf)
+fi
+
+if [[ $selected == "/exp/$(whoami)" ]] ; then
+    sesh_name="exp"
+else
+    sesh_name=$(basename "$selected" | tr . _)
 fi
 
 if [[ -z $selected ]]; then
@@ -21,19 +28,18 @@ if tmux info &> /dev/null; then
     is_inside="true"
 fi
 
-selected_name=$(basename "$selected" | tr . _)
 
 if [[ -z $TMUX ]] && [[  -z $is_running ]]; then
-    tmux new-session -s $selected_name -c $selected
+    tmux new-session -s $sesh_name -c $selected
     exit 0
 fi
 
-if ! tmux has-session -t=$selected_name 2> /dev/null; then
-    tmux new-session -ds $selected_name -c $selected
+if ! tmux has-session -t=$sesh_name 2> /dev/null; then
+    tmux new-session -ds $sesh_name -c $selected
 fi
 
 if [[ -z $is_inside ]]; then
-    tmux attach -t $selected_name
+    tmux attach -t $sesh_name
 fi
 
-tmux switch-client -t $selected_name
+tmux switch-client -t $sesh_name
