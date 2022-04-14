0a3fd86a53d4254ec649882291fd9584dcb1128c
erees1 <edward.r.rees@zoho.com>
2022-04-14 18:57:33 +0100

    hide nv tree folder arrows

--- vim/lua/plugins/nv-tree.lua
+++ vim/lua/plugins/nv-tree.lua
@@ -1,9 +1,14 @@
-vim.api.nvim_set_var('nvim_tree_indent_markers',1)
 vim.g.nvim_tree_icons = {
     default = '',
     symlink = '',
     git = {unstaged = "", staged = "✓", unmerged = "", renamed = "➜", untracked = ""},
     folder = {default = "", open = "", empty = "", empty_open = "", symlink = ""}
+}
+vim.g.nvim_tree_show_icons = {
+    git= 1,
+    folders= 1,
+    files= 1,
+    folder_arrows= 0,
 }
 
 require'nvim-tree'.setup {
