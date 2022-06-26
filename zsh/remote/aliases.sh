dcd89d6f231da586e0b95c3b981f7030ba713367
edwardr <edwardr@speechmatics.com>
2022-06-26 21:59:42 +0100

    cleaner q funcs

--- zsh/remote/aliases.sh
+++ zsh/remote/aliases.sh
@@ -75,50 +75,50 @@ alias dcl='docker container ls'
 alias tb="singularity exec $TENSOR_BOARD_SIF tensorboard --host=$HOST_IP_ADDR --reload_multifile true --logdir=."
 
 tblink () {
-  # Creates simlinks from specified folders to ~/tb/x where x is an incrmenting number
-  # and luanches tensorboard
-  # example: `tblink ./lm/20210824 ./lm/20210824_ablation ./lm/20210825_updated_data`
-  if [ "$#" -eq 0 ]; then
-    logdir=$(pwd)
-  else
-  # setup tensorboard directory
-    tbdir="$HOME/tb"
-    if [ -d "$tbdir" ]; then
-      last="$(printf '%s\n' $tbdir/* | sed 's/.*\///' | sort -g -r | head -n 1)"
-      new=$((last+1))
-      echo "last folder $last, new folder $new"
-      logdir="$tbdir/$new"
+    # Creates simlinks from specified folders to ~/tb/x where x is an incrmenting number
+    # and luanches tensorboard
+    # example: `tblink ./lm/20210824 ./lm/20210824_ablation ./lm/20210825_updated_data`
+    if [ "$#" -eq 0 ]; then
+        logdir=$(pwd)
     else
-      logdir="$tbdir/0"
+        # setup tensorboard directory
+        tbdir="$HOME/tb"
+        if [ -d "$tbdir" ]; then
+            last="$(printf '%s\n' $tbdir/* | sed 's/.*\///' | sort -g -r | head -n 1)"
+            new=$((last+1))
+            echo "last folder $last, new folder $new"
+            logdir="$tbdir/$new"
+        else
+            logdir="$tbdir/0"
+        fi
+        # softlink into tensorboard directory
+        _linkdirs "$logdir" "$@"
     fi
-    # softlink into tensorboard directory
-    _linkdirs "$logdir" "$@"
-  fi
-  singularity exec "$TENSOR_BOARD_SIF" tensorboard --host=$HOST_IP_ADDR --reload_multifile true --logdir=$logdir
+    singularity exec "$TENSOR_BOARD_SIF" tensorboard --host=$HOST_IP_ADDR --reload_multifile true --logdir=$logdir
 }
 _linkdirs() {
     logdir="$1"
     mkdir -p $logdir
     for linkdir in "${@:2}"; do
-      linkdir=$(readlink -f $linkdir)
-      if [ ! -d $linkdir ]; then
-          echo "linkdir $linkdir does not exist"
-          return
-      fi
-      echo "symlinked $linkdir into $logdir"
-      ln -s $linkdir $logdir
+        linkdir=$(readlink -f $linkdir)
+        if [ ! -d $linkdir ]; then
+            echo "linkdir $linkdir does not exist"
+            return
+        fi
+        echo "symlinked $linkdir into $logdir"
+        ln -s $linkdir $logdir
     done
 }
 tbadd() {
-  # Add experiment folder to existing tensorboard directory (see tblink)
-  # example: `tbadd ./lm/20210825 25` will symlink ./lm/20210824 to ~/tb/25
-  if [ "$#" -gt 1 ]; then
-    tbdir="$HOME/tb"
-    logdir=$tbdir/$1
-    _linkdirs $logdir "${@:2}"
-  else
-    echo "tbadd <tb number> <exp dirs>"
-  fi
+    # Add experiment folder to existing tensorboard directory (see tblink)
+    # example: `tbadd ./lm/20210825 25` will symlink ./lm/20210824 to ~/tb/25
+    if [ "$#" -gt 1 ]; then
+        tbdir="$HOME/tb"
+        logdir=$tbdir/$1
+        _linkdirs $logdir "${@:2}"
+    else
+        echo "tbadd <tb number> <exp dirs>"
+    fi
 }
 
 # -------------------------------------------------------------------
@@ -142,38 +142,33 @@ alias qtl="qtail -l"
 
 # Queue functions
 qlogin () {
-  # Function to request gpu or cpu access
-  # example:
-  #    qlogin 2                request 2 gpus
-  #    qlogin 1 cpu            request 1 cpu slot
-  #    qlogin 1 aml-gpu.q@b5   request 1 gpu on b5
-  if [ "$#" -eq 1 ]; then
-    /usr/bin/qlogin -now n -pe smp $1 -q aml-gpu.q -l gpu=$1 -N D_$(whoami)
-  elif [ "$#" -eq 2 ]; then
-    gpu_args=""
-    if [ "$2" = "cpu" ]; then
-      queue="aml-cpu.q"
-    elif  echo "$2" | grep -q "gpu" ; then
-      queue="$2"
-      gpu_args="gpu=$1"
+    # Function to request gpu or cpu access
+    # example:
+    #    qlogin 2                request 2 gpus
+    #    qlogin 1 cpu            request 1 cpu slot
+    #    qlogin 1 aml-gpu.q@b5   request 1 gpu on b5
+    if [ "$#" -eq 1 ]; then
+        /usr/bin/qlogin -now n -pe smp $1 -q aml-gpu.q -l gpu=$1 -N D_$(whoami)
+    elif [ "$#" -eq 2 ]; then
+        gpu_args=""
+        if [ "$2" = "cpu" ]; then
+            queue="aml-cpu.q"
+        elif  echo "$2" | grep -q "gpu" ; then
+            queue="$2"
+            gpu_args="gpu=$1"
+        else
+            queue="$2"
+        fi
+        /usr/bin/qlogin -now n -pe smp $1 -q $queue -l "$gpu_args" -N D_$(whoami)
     else
-      queue="$2"
+        echo "Usage: qlogin <num_gpus>" >&2
+        echo "Usage: qlogin <num_gpus> <queue>" >&2
+        echo "Usage: qlogin <num_slots> cpu" >&2
     fi
-    /usr/bin/qlogin -now n -pe smp $1 -q $queue -l "$gpu_args" -N D_$(whoami)
-  else
-    echo "Usage: qlogin <num_gpus>" >&2
-    echo "Usage: qlogin <num_gpus> <queue>" >&2
-    echo "Usage: qlogin <num_slots> cpu" >&2
-  fi
 }
 qtail () {
     if [ "$#" -gt 0 ]; then
-        b=$(qlog $@)
-        if [ ! -z "$b" ]; then
-            tail -f $b
-        else
-            echo "log file not found"
-        fi
+        l=$(qlog $@) && tail -f $l
     else
         echo "Usage: qtail <jobid>" >&2
         echo "Usage: qtail <array_jobid> <sub_jobid>" >&2
@@ -181,19 +176,19 @@ qtail () {
     fi
 }
 qlast () {
-    # Tail the last running job
+    # Get job_id of last running job
     job_id=$(qstat | awk '$5=="r" {print $1}' | grep -E '[0-9]' | sort -r | head -n 1)
     if [ ! -z $job_id ]; then
         echo $job_id
     else
-        echo "no jobs found"
+        echo "no jobs found" >&2
     fi
 }
 qless () {
     less $(qlog $@)
 }
 qcat () {
-    cat $(qlog $@)
+    l=$(qlog $@) && cat $l
 }
 echo_if_exist() {
     [ -f $1 ] && echo $1
@@ -218,11 +213,12 @@ qlog () {
         base_dir=$(echo $log_path | rev | cut -d "/" -f3- | rev)
         filename=$(basename $log_path)
         # Could be a number of schemes so just try them all
-        echo_if_exist ${base_dir}/log/${filename}
-        echo_if_exist ${base_dir}/log/${filename%.log}${2}.log
-        echo_if_exist ${base_dir}/log/${filename%.log}.${2}.log
-        echo_if_exist ${base_dir}/${filename%.log}.${2}.log
-        echo_if_exist ${base_dir}/${filename%.log}${2}.log
+        echo_if_exist ${base_dir}/log/${filename} && return 0
+        echo_if_exist ${base_dir}/log/${filename%.log}${2}.log && return 0
+        echo_if_exist ${base_dir}/log/${filename%.log}.${2}.log && return 0
+        echo_if_exist ${base_dir}/${filename%.log}.${2}.log  && return 0
+        echo_if_exist ${base_dir}/${filename%.log}${2}.log && return 0
+        echo "log file for array job $job_id $2 not found" >&2 && return 1
     else
         echo "Usage: qlog <jobid>" >&2
         echo "Usage: qlog <array_jobid> <sub_jobid>" >&2
