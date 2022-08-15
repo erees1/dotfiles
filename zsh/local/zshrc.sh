76b998197a16c17a84a5bd718b604007c6eb7ce1
edwardr <edwardr@speechmatics.com>
2022-08-15 11:38:12 +0100

    remove redundant code / comments

--- zsh/local/zshrc.sh
+++ zsh/local/zshrc.sh
@@ -4,7 +4,7 @@ export LOC="local"
 # Local aliases
 alias vm='ssh vm'
 alias rl='greadlink -f'
-alias cdm='cd ~/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/md-notes'
+alias cdm='cd ~/git/md-notes'
 alias cdj='cd ~/git/jamming'
 alias cdn='cd ~/git/jamming/notebooks'
 
@@ -15,14 +15,6 @@ source $ZSH_DOT_DIR/local/dir_colors.sh
 source $ZSH_DOT_DIR/common/zshrc.sh
 zstyle ':completion:*' list-colors $LS_COLORS
 
-# When /etc/profile is run we make sure that the path is empty
-# this prevents tmux from screwing with my path 
-# seeing if removing this affects anything
-#if [ -f /etc/profile ] ; then
-    #TMPPATH=$PATH
-    #source /etc/profile
-    #PATH=$TMPPATH
-#fi
 # Need to remember to install miniconda to opt
 conda_loc="${HOME}/opt/miniconda3"
 if command -v conda 2>/dev/null 2>&1; then
@@ -44,8 +36,6 @@ if command -v conda 2>/dev/null 2>&1; then
       # <<< conda initialize <<<
     fi
 fi
-
-
 
 # inserts shim for .rbenv if its not there already
 if hash rbenv 2>/dev/null; then 
