c6766361dcf8129321a563e8d41280838c73f16b
edwardr <edwardr@speechmatics.com>
2022-09-29 13:51:27 +0100

    fix zsh vi mode cursor sometimes getting stuck in normal mode

--- zsh/common/keybindings.sh
+++ zsh/common/keybindings.sh
@@ -10,24 +10,20 @@ KEYTIMEOUT=5
 function zle-keymap-select {
   if [[ ${KEYMAP} == vicmd ]] ||
      [[ $1 = 'block' ]]; then
-    echo -ne '\e[2 q'
+    echo -ne '\e[1 q'
 
   elif [[ ${KEYMAP} == main ]] ||
        [[ ${KEYMAP} == viins ]] ||
        [[ ${KEYMAP} = '' ]] ||
        [[ $1 = 'beam' ]]; then
-    echo -ne '\e[6 q'
+    echo -ne '\e[5 q'
   fi
 }
 zle -N zle-keymap-select
-
-# Use beam shape cursor on startup.
-echo -ne '\e[6 q'
-
-# Use beam shape cursor for each new prompt.
-precmd() {
-   echo -ne '\e[6 q'
+_fix_cursor() {
+   echo -ne '\e[5 q'
 }
+precmd_functions+=(_fix_cursor)
 
 # git add ci and push
 function git_prepare() {
