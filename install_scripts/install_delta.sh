a80af474ba6fc6f23519152b0a8e8f8c937d6609
edwardr <edwardr@speechmatics.com>
2023-02-24 10:57:46 +0000

    remove duplicate rm

--- install_scripts/install_delta.sh
+++ install_scripts/install_delta.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 set -euo pipefail
 
-rm -rf $HOME/.local/delta $HOME/.local/bin/delta
+rm -rf $HOME/.local/delta
 url="https://github.com/dandavison/delta/releases/download/0.4.4/git-delta_0.4.4_amd64.deb"
 
 $DOT_DIR/install_scripts/install_deb.sh "$url" "delta"
