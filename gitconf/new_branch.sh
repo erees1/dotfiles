5719ebf9029bcef33c6f19ecc54269763e493a0e
edwardr <edwardr@speechmatics.com>
2023-03-23 13:45:00 +0000

    update new_branch.sh

--- gitconf/new_branch.sh
+++ gitconf/new_branch.sh
@@ -4,9 +4,9 @@ set -eo pipefail
 . $DOT_DIR/zsh/utils.sh
 
 cat << EOF > /tmp/git-script.sh
-git fetch origin master:master
 git branch $1 master
 git checkout $1
+git pull origin master
 git push --set-upstream origin $1
 EOF
 
