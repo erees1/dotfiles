f77a65fb23b0c77dba73f547e86b5456a5bff148
edwardr <edwardr@speechmatics.com>
2023-04-06 15:34:37 +0100

    add fzf/bin to front of path

--- zsh/remote/zshrc.sh
+++ zsh/remote/zshrc.sh
@@ -9,6 +9,7 @@ export PYENV_ROOT="$HOME/.pyenv/bin"
 add_to_path $PYENV_ROOT
 add_to_path "$MY_BIN_LOC"
 add_to_path "${HOME}/.npm-global/bin"
+add_to_path "${HOME}/.fzf/bin"  # Ensure at start of path
 
 . $ZSH_DOT_DIR/common/zshrc.sh
 . $ZSH_DOT_DIR/remote/aliases.sh
