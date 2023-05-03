0366f025266c969a7dc803b44bb237862736e40c
edwardr <edwardr@speechmatics.com>
2023-05-03 11:38:27 +0100

    Fix oscyank

--- vim/lua/remote.lua
+++ vim/lua/remote.lua
@@ -1,21 +1,22 @@
  vim.api.nvim_command([[
-autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '+' | OSCYankReg + | endif
+autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '+' | execute 'OSCYankRegister +' | endif
 ]])
  vim.api.nvim_command([[
-autocmd TextYankPost * if v:event.operator is 'x' && v:event.regname is '+' | OSCYankReg + | endif
+autocmd TextYankPost * if v:event.operator is 'x' && v:event.regname is '+' | execute 'OSCYankRegister +' | endif
 ]])
 
 vim.api.nvim_command([[
 function! YankFullPathToOsc()
 let @+ = expand('%:p')
-OSCYankReg +
+OSCYankRegister +
 endfunction
 
 function! YankRelativePathToOsc()
 let @+ = expand('%:.')
-OSCYankReg +
+OSCYankRegister +
 endfunction
 ]])
 
 vim.api.nvim_set_keymap('n', '<leader>yr' , ':call YankRelativePathToOsc()<CR>', {noremap=true, silent=true })
 vim.api.nvim_set_keymap('n', '<leader>yf' , ':call YankFullPathToOsc()<CR>', {noremap=true, silent=true })
+
