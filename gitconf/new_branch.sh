4b7db6bb2abb12c0e88bc78d45bf2dad83beaaa0
edwardr <edwardr@speechmatics.com>
2023-06-15 19:38:15 +0100

    only stash pop if something was saved

--- gitconf/new_branch.sh
+++ gitconf/new_branch.sh
@@ -4,14 +4,19 @@ set -eo pipefail
 . $DOT_DIR/zsh/utils.sh
 
 cat << EOF > /tmp/git-script.sh
-git stash
+STASH_MSG=\$(git stash)
 git branch $1 master
 git checkout $1
 git pull origin master
 git push --set-upstream origin $1
-git stash pop
+
+# Only pop the stash if something was stashed
+if [[ \$STASH_MSG != "No local changes to save" ]]; then
+  git stash pop
+fi
 EOF
 
 chmod u+x /tmp/git-script.sh
 $(maybe_singularity_exec) /tmp/git-script.sh
 rm /tmp/git-script.sh
+
