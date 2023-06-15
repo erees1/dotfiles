6cb922a5ded9733fbc04ff111f3fec2c76648956
edwardr <edwardr@speechmatics.com>
2023-06-15 19:33:59 +0100

    stash changes in git new-branch

--- gitconf/new_branch.sh
+++ gitconf/new_branch.sh
@@ -4,10 +4,12 @@ set -eo pipefail
 . $DOT_DIR/zsh/utils.sh
 
 cat << EOF > /tmp/git-script.sh
+git stash
 git branch $1 master
 git checkout $1
 git pull origin master
 git push --set-upstream origin $1
+git stash pop
 EOF
 
 chmod u+x /tmp/git-script.sh
