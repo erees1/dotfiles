dd7fb705bc49b91f3761cb7982e6e873e0c9a23e
erees1 <edward.r.rees@gmail.com>
2023-09-09 20:21:39 +0100

    add tmux plugin manager

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,4 +1,9 @@
 if [ "$(uname -s)" == "Darwin" ]
 then
   brew install tmux
+
+  # Install tmux plugin manager
+  if [ ! -d ~/.tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  fi
 fi
