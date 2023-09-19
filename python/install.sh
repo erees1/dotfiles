db21f9b17cc7d0a302ee6e7bcaaf85e6b9f31316
erees1 <edward.r.rees@gmail.com>
2023-09-19 11:34:37 +0100

    add pyenv brew dependencies

--- python/install.sh
+++ python/install.sh
@@ -1,5 +1,7 @@
 if [ "$(uname -s)" == "Darwin" ]
 then
+  # as per https://github.com/pyenv/pyenv/wiki#how-to-build-cpython-with-framework-support-on-os-x
+  brew install openssl readline sqlite3 xz zlib tcl-tk
   brew install pyenv
   brew install pyenv-virtualenv
 fi
