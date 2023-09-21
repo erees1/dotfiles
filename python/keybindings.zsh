bb0205b0d6ed066affdff97104feca79d28f31e1
erees1 <edward.r.rees@gmail.com>
2023-09-21 22:08:45 +0100

    put cursor back at end of line after calling in python debug keybinding

--- python/keybindings.zsh
+++ python/keybindings.zsh
@@ -2,8 +2,10 @@
 function replace-python {
   if [[ $BUFFER =~ ^python\ .* ]]; then
     BUFFER="python3 -m debugpy --listen 5678 --wait-for-client ${BUFFER#python }"
+    CURSOR+=51
     zle reset-prompt
   fi
 }
 zle -N replace-python
 bindkey "\ed" replace-python
+
