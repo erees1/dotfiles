22cfb8cdd8577bcc6ce09c80092d80caf2e6945c
erees1 <edward.r.rees@gmail.com>
2023-09-21 22:07:40 +0100

    add autoreload auto command

--- nvim.config/lua.symlink/settings.lua
+++ nvim.config/lua.symlink/settings.lua
@@ -49,3 +49,24 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt_local.ts = 2
     end,
 })
+
+-- Triger `autoread` when files changes on disk
+-- https://unix.stackexchange.com/questions/149209/refresh-changed-content-of-file-opened-in-vim/383044#383044
+-- https://vi.stackexchange.com/questions/13692/prevent-focusgained-autocmd-running-in-command-line-editing-mode
+vim.o.autoread = true
+vim.api.nvim_exec([[
+  augroup AutoReadGroup
+    autocmd!
+    autocmd FocusGained,BufEnter,CursorHold,CursorHoldI * if mode() !~# '\v(c|r.?|!|t)' && getcmdwintype() == '' | checktime | endif
+  augroup END
+]], false)
+
+-- Notification after file change
+-- https://vi.stackexchange.com/questions/13091/autocmd-event-for-autoread
+vim.api.nvim_exec([[
+  augroup NotifyFileChangeGroup
+    autocmd!
+    autocmd FileChangedShellPost * echohl WarningMsg | echo "File changed on disk. Buffer reloaded." | echohl None
+  augroup END
+]], false)
+
