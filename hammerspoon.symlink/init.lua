653cb818aa6da53956374f4f0ab3297054d41fab
erees1 <edward.r.rees@gmail.com>
2023-09-29 11:00:10 +0100

    add move window to next screen and obsidian binding

--- hammerspoon.symlink/init.lua
+++ hammerspoon.symlink/init.lua
@@ -1,21 +1,21 @@
-
 hs.window.animationDuration = 0;
-modifiers = {"ctrl", "cmd"}
+local modifiers = { "ctrl", "cmd" }
 
-function bindHotkeys(mapping)
-  for key, app in pairs(mapping) do
-    hs.hotkey.bind(modifiers, key, function()
-      hs.application.launchOrFocus(app)
-    end)
-  end
+local function bindHotkeys(mapping)
+    for key, app in pairs(mapping) do
+        hs.hotkey.bind(modifiers, key, function()
+            hs.application.launchOrFocus(app)
+        end)
+    end
 end
 
-mapping = {
-  i = "iTerm",
-  e = "Google Chrome",
-  m = "Spark Desktop",
-  a = "Sublime Text",
-  s = "Slack",
+local mapping = {
+    i = "iTerm",
+    e = "Google Chrome",
+    m = "Spark Desktop",
+    a = "Visual Studio Code",
+    s = "Slack",
+    o = "Obsidian",
 }
 
 bindHotkeys(mapping)
@@ -57,21 +57,35 @@ hs.hotkey.bind(modifiers, "return", function()
     win:setFrame(max)
 end)
 
+-- Move window to next screen
+hs.hotkey.bind(modifiers, ";", function()
+    local win = hs.window.focusedWindow()
+    win:moveToScreen(win:screen():next())
+    -- set height to full (otherwise after moving it gets truncated vertically)
+    -- may not want this if you are every splitting vertically (I don't atm)
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+    f.h = max.h
+    win:setFrame(f)
+end)
+
+
 -- Layouts
 local windowLayout = {
-    {"Sublime Text",  nil,          laptopScreen, hs.layout.left50,    nil, nil},
-    {"iTerm",    nil,          laptopScreen, hs.layout.right50,   nil, nil},
-    {"Google Chrome",    nil,          laptopScreen, hs.layout.right50,   nil, nil},
-    {"Code",  nil,     laptopScreen, hs.layout.left50, nil, nil},
+    { "Visual Studio Code", nil, laptopScreen, hs.layout.left50,  nil, nil },
+    { "iTerm",              nil, laptopScreen, hs.layout.right50, nil, nil },
+    { "Google Chrome",      nil, laptopScreen, hs.layout.right50, nil, nil },
+    { "Code",               nil, laptopScreen, hs.layout.left50,  nil, nil },
 }
 hs.hotkey.bind(modifiers, "r", function()
     hs.layout.apply(windowLayout)
 end)
 
 -- Automatically reload config
-function reloadConfig(files)
+local function reloadConfig(files)
     doReload = false
-    for _,file in pairs(files) do
+    for _, file in pairs(files) do
         if file:sub(-4) == ".lua" then
             doReload = true
         end
@@ -80,5 +94,6 @@ function reloadConfig(files)
         hs.reload()
     end
 end
+
 myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.alert.show("Config loaded")
