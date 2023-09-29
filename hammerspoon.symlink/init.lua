eb3b3a452d356e1b9cf24ce3a2fae6f0228ec53b
erees1 <edward.r.rees@gmail.com>
2023-09-29 20:11:13 +0100

    update spoons

--- hammerspoon.symlink/init.lua
+++ hammerspoon.symlink/init.lua
@@ -1,25 +1,13 @@
 hs.window.animationDuration = 0;
 local modifiers = { "ctrl", "cmd" } -- all shortcuts use this
+local window_margin = 5             -- border to have around windows, set to 0 if you don't want any
 
-local function bindHotkeys(mapping)
-    for key, app in pairs(mapping) do
-        hs.hotkey.bind(modifiers, key, function()
-            hs.application.launchOrFocus(app)
-        end)
+local function launchApp(App)
+    return function()
+        hs.application.launchOrFocus(App)
     end
 end
 
-local app_open_mapping = {
-    i = "iTerm",
-    e = "Google Chrome",
-    m = "Spark Desktop",
-    a = "Visual Studio Code",
-    s = "Slack",
-    o = "Obsidian",
-    s = "Finder",
-}
-
-bindHotkeys(app_open_mapping)
 
 local function baseMove(x, y, w, h)
     return function()
@@ -29,15 +17,60 @@ local function baseMove(x, y, w, h)
         local max = screen:frame()
 
         -- add max.x so it stays on the same screen, works with my second screen
-        f.x = max.w * x + max.x
-        f.y = max.h * y
-        f.w = max.w * w
-        f.h = max.h * h
+        f.x = max.w * x + max.x + window_margin
+        f.y = max.h * y + window_margin + max.y
+        f.w = max.w * w - window_margin / 2
+        f.h = max.h * h - window_margin
+        print('setting frame', f.x, f.y, f.w, f.h)
         win:setFrame(f, 0)
     end
 end
 
+local snapLeft = baseMove(0, 0, 0.5, 1)
+local snapRight = baseMove(0.5, 0, 0.5, 1)
+local snapTop = baseMove(0, 0, 1, 0.5)
+local snapBottom = baseMove(0, 0.5, 1, 0.5)
+local snapMax = baseMove(0, 0, 1, 1)
+
 local stashed_windows = {}
+
+
+local function moveWindow(direction)
+    return function()
+        local win = hs.window.focusedWindow()
+        local screen = win:screen()
+        local max = screen:frame()
+
+
+        -- work out if we are already on the left or right by moving it and
+        -- checking if the x position or width has changed
+        local curFrame = win:frame()
+        if (direction == "left") then
+            snapLeft()
+        else
+            snapRight()
+        end
+        local didNotMove = (curFrame.x == win:frame().x and curFrame.w == win:frame().w and curFrame.y == win:frame().y and curFrame.h == win:frame().h)
+
+        if not didNotMove then
+            -- if we did move then we are done so return
+            return
+        end
+
+        -- if we didn't move then we are already on the left or right so need to move to the next screen
+        local win = hs.window.focusedWindow()
+        if (direction == "right") then
+            local nextScreen = win:screen():next()
+            win:moveToScreen(nextScreen)
+            snapLeft()
+        else
+            local nextScreen = win:screen():previous()
+            win:moveToScreen(nextScreen)
+            snapRight()
+        end
+    end
+end
+
 
 local function maximizeWindow()
     -- maximize window and remember original size
@@ -48,55 +81,26 @@ local function maximizeWindow()
     local max = screen:frame()
 
     -- if stashed window and currently maximized, restore
-    if stashed_windows[win:id()] and f.x == max.x and f.y == max.y and f.w == max.w and f.h == max.h then
-        f.x = stashed_windows[win:id()].x
-        f.y = stashed_windows[win:id()].y
-        f.w = stashed_windows[win:id()].w
-        f.h = stashed_windows[win:id()].h
-        stashed_windows[win:id()] = nil
+    local curFrame = win:frame()
+    print("calling maximize")
+    snapMax()
+    local didNotMove = (curFrame.x == win:frame().x and curFrame.w == win:frame().w and curFrame.y == win:frame().y and curFrame.h == win:frame().h)
+    if didNotMove then
+        -- then restore
+        frame = stashed_windows[win:id()]
+        if frame then
+            f.x = frame.x
+            f.y = frame.y
+            f.w = frame.w
+            f.h = frame.h
+            stashed_windows[win:id()] = nil
+            win:setFrame(f, 0)
+        end
     else
-        stashed_windows[win:id()] = {
-            x = f.x,
-            y = f.y,
-            w = f.w,
-            h = f.h,
-        }
-        f.x = max.x
-        f.y = max.y
-        f.w = max.w
-        f.h = max.h
+        -- then stash
+        stashed_windows[win:id()] = curFrame
     end
-    win:setFrame(f, 0)
 end
-
-
--- feature spectacle/another window sizing apps
-hs.hotkey.bind(modifiers, 'h', baseMove(0, 0, 0.5, 1))
-hs.hotkey.bind(modifiers, 'l', baseMove(0.5, 0, 0.5, 1))
-hs.hotkey.bind(modifiers, 'j', baseMove(0, 0.5, 1, 0.5))
-hs.hotkey.bind(modifiers, 'k', baseMove(0, 0, 1, 0.5))
-hs.hotkey.bind(modifiers, '1', baseMove(0, 0, 0.5, 0.5))
-hs.hotkey.bind(modifiers, '2', baseMove(0.5, 0, 0.5, 0.5))
-hs.hotkey.bind(modifiers, '3', baseMove(0, 0.5, 0.5, 0.5))
-hs.hotkey.bind(modifiers, '4', baseMove(0.5, 0.5, 0.5, 0.5))
-hs.hotkey.bind(modifiers, 'return', maximizeWindow)
-
-
--- Move window to next screen
-hs.hotkey.bind(modifiers, ";", function()
-    local win = hs.window.focusedWindow()
-    win:moveToScreen(win:screen():next())
-    -- set height to full (otherwise after moving it gets truncated vertically)
-    -- may not want this if you are every splitting vertically (I don't atm)
-    local f = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
-    f.h = max.h
-    win:setFrame(f)
-    -- clear stashed window when moving screen
-    stashed_windows[win:id()] = nil
-end)
-
 
 -- Layouts
 local windowLayout = {
@@ -104,25 +108,28 @@ local windowLayout = {
     { "iTerm",              nil, laptopScreen, hs.layout.right50, nil, nil },
     { "Google Chrome",      nil, laptopScreen, hs.layout.right50, nil, nil },
     { "Code",               nil, laptopScreen, hs.layout.left50,  nil, nil },
+    { "Obsidian",           nil, laptopScreen, hs.layout.left50,  nil, nil },
 }
-hs.hotkey.bind(modifiers, "r", function()
-    hs.layout.apply(windowLayout)
-end)
 
+function applyLayout(layout)
+    return function()
+        hs.layout.apply(layout)
+    end
+end
 
 -- Execute last  command in iterm
-hs.hotkey.bind(modifiers, "p", function()
+local function executeLastCommand()
     -- Save the current focused window
     local currentWindow = hs.window.focusedWindow()
 
     -- Focus iTerm
     hs.application.launchOrFocus("iTerm")
-    
+
     -- Give iTerm some time to focus before sending the keys
     hs.timer.doAfter(0.01, function()
         -- Send up arrow to get the last command
         hs.eventtap.keyStroke({}, "up")
-        
+
         -- Send return to run the last command
         hs.eventtap.keyStroke({}, "return")
 
@@ -134,23 +141,43 @@ hs.hotkey.bind(modifiers, "p", function()
             end
         end)
     end)
-end)
-
-
-
-
--- Automatically reload config
-local function reloadConfig(files)
-    doReload = false
-    for _, file in pairs(files) do
-        if file:sub(-4) == ".lua" then
-            doReload = true
-        end
-    end
-    if doReload then
-        hs.reload()
-    end
 end
 
-myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
-hs.alert.show("Config loaded")
+
+local function reloadConfig()
+    hs.reload()
+    hs.notify.new({ title = "Hammerspoon", informativeText = "Config loaded" }):send()
+end
+
+-- Keybindings
+local keybindings = {
+    -- Apps
+    { key = 'i',      fn = launchApp("iTerm") },
+    { key = 'e',      fn = launchApp("Google Chrome") },
+    { key = 'a',      fn = launchApp("Visual Studio Code") },
+    { key = 'm',      fn = launchApp("Spark Desktop") },
+    { key = 's',      fn = launchApp("Slack") },
+    { key = 'o',      fn = launchApp("Obsidian") },
+    { key = 'r',      fn = reloadConfig },
+
+    -- Windows
+    { key = 'h',      fn = moveWindow("left") },
+    { key = 'l',      fn = moveWindow("right") },
+    { key = 'j',      fn = snapBottom },
+    { key = 'k',      fn = snapTop },
+    { key = "1",      fn = baseMove(0, 0, 0.5, 0.5) },
+    { key = "2",      fn = baseMove(0.5, 0, 0.5, 0.5) },
+    { key = "3",      fn = baseMove(0, 0.5, 0.5, 0.5) },
+    { key = "4",      fn = baseMove(0.5, 0.5, 0.5, 0.5) },
+    { key = 'return', fn = maximizeWindow },
+    { key = 'r',      fn = applyLayout },
+
+    -- misc
+    { key = 'p',  fn = executeLastCommand },
+    { key = '0', fn = reloadConfig}
+}
+
+-- Bind the hotkeys
+for _, binding in pairs(keybindings) do
+    hs.hotkey.bind(modifiers, binding.key, binding.fn)
+end
