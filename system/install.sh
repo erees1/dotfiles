629a376e20bf815c422884cc9f90cce710a02ae9
erees1 <edward.r.rees@gmail.com>
2023-10-01 15:13:57 +0100

    add htop to system install.sh

--- system/install.sh
+++ system/install.sh
@@ -1,11 +1,12 @@
 #! /usr/bin/env zsh
 SRC_DIR=$(dirname "$0")
 
-# System tools
+# System tools / utilities
 if [ "$(uname -s)" = "Darwin" ]
 then
   brew install exa
   brew install fzf
+  brew install htop
 else
   sudo apt install exa
   sudo apt install fzf
