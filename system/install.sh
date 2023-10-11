085d7947e3a90cd123cbdfcdaccb607fb0bda626
erees1 <edward.r.rees@gmail.com>
2023-10-11 14:11:57 +0100

    on fzf install install keybindings to ~

--- system/install.sh
+++ system/install.sh
@@ -7,6 +7,7 @@ then
   brew install exa
   brew install fzf
   brew install htop
+  $(brew --prefix)/opt/fzf/install --all
 else
   # Just use the no root versions so that e.g. fzf uses a later version 
   $SRC_DIR/install_no_root.sh
