bcaab41556544b0270a66e2b3df4fcaf1d46ae50
erees1 <edward.r.rees@gmail.com>
2023-10-15 11:36:10 +0100

    acutally add luasnip to dependencies

--- nvim.config/lua.symlink/plugins/init.lua
+++ nvim.config/lua.symlink/plugins/init.lua
@@ -58,7 +58,6 @@ local plugin_list = {
             "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-nvim-lsp",
             "L3MON4D3/LuaSnip",
-            "saadparwaiz1/cmp_luasnip",
             "hrsh7th/cmp-nvim-lua",
             "hrsh7th/cmp-path",
             "hrsh7th/cmp-nvim-lsp-signature-help",
