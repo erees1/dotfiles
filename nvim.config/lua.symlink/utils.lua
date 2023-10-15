ad5342e25d354c47446d5c7cbc401f247bbf7ac3
erees1 <edward.r.rees@gmail.com>
2023-10-15 11:35:49 +0100

    remove reset_bufferline func

--- nvim.config/lua.symlink/utils.lua
+++ nvim.config/lua.symlink/utils.lua
@@ -1,10 +1,6 @@
 local M = {}
 function M.is_not_vscode()
-	return vim.api.nvim_eval('exists("g:vscode")') == 0
-end
-
-function M.reset_bufferline()
-	require("bufferline.api").set_offset(0)
+    return vim.api.nvim_eval('exists("g:vscode")') == 0
 end
 
 function M.tree_toggle()
