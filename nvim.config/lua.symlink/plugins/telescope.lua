cfa075cd7986a7995cdc3c4f233cd77744429d50
erees1 <edward.r.rees@gmail.com>
2024-01-06 15:44:19 +0000

    moar telescope

--- nvim.config/lua.symlink/plugins/telescope.lua
+++ nvim.config/lua.symlink/plugins/telescope.lua
@@ -5,7 +5,6 @@ local actions = require("telescope.actions")
 require("telescope").setup({
     defaults = {
         file_ignore_patterns = {
-            ".git/",
             ".cache",
             "%.o",
             "%.a",
@@ -35,11 +34,11 @@ require("telescope").setup({
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "TelescopePrompt",
-  callback = function()
-    -- exit on esc when in insert mode
-    vim.keymap.set("i", "<ESC>", "<ESC>:q!<cr>", { noremap = true, silent = true, buffer=true })
-  end,
+    pattern = "TelescopePrompt",
+    callback = function()
+        -- exit on esc when in insert mode
+        vim.keymap.set("i", "<ESC>", "<ESC>:q!<cr>", { noremap = true, silent = true, buffer = true })
+    end,
 })
 
 -- Enable telescope fzf native, if installed
@@ -56,9 +55,11 @@ vim.keymap.set("n", "<leader>/", function()
     }))
 end, { desc = "[/] Fuzzily search in current buffer]" })
 
-vim.keymap.set("n", "<leader>t", require("telescope.builtin").find_files,   { desc = "[T]elescope" })
-vim.keymap.set("n", "<leader>of", require("telescope.builtin").find_files,  { desc = "Telescp[o]pe [F]iles" })
-vim.keymap.set("n", "<leader>oh", require("telescope.builtin").help_tags,   { desc = "Telescp[o]pe [H]elp" })
+vim.keymap.set("n", "<leader>t", require("telescope.builtin").git_files, { desc = "[T]elescope" })
+vim.keymap.set("n", "<leader>of", require("telescope.builtin").find_files, { desc = "Telescp[o]pe [F]iles" })
+vim.keymap.set("n", "<leader>oh", require("telescope.builtin").help_tags, { desc = "Telescp[o]pe [H]elp" })
 vim.keymap.set("n", "<leader>ow", require("telescope.builtin").grep_string, { desc = "Telescp[o]pe current [W]ord" })
-vim.keymap.set("n", "<leader>og", require("telescope.builtin").live_grep,   { desc = "Telescp[o]pe by [G]rep" })
+vim.keymap.set("n", "<leader>og", require("telescope.builtin").live_grep, { desc = "Telescp[o]pe by [G]rep" })
 vim.keymap.set("n", "<leader>od", require("telescope.builtin").diagnostics, { desc = "Telescp[o]pe [D]iagnostics" })
+vim.keymap.set("n", "<leader>os", require("telescope.builtin").lsp_dynamic_workspace_symbols, { desc = "Telescp[o]pe [S]ymbols" })
+vim.keymap.set("n", "<leader>or", require("telescope.builtin").lsp_references, { desc = "Telescp[o]pe [S]ymbols" })
