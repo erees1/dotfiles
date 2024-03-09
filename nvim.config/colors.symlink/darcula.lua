1ec01d508b7aabde72778a8166df100719b007a9
erees1 <edward.r.rees@gmail.com>
2024-03-09 15:57:50 +0000

    diff colors

--- nvim.config/colors.symlink/darcula.lua
+++ nvim.config/colors.symlink/darcula.lua
@@ -3,17 +3,19 @@ vim.g.colors_name = "darcula-solid-custom"
 
 local lush = require("lush")
 local hsl = lush.hsl
+local hex = lush.hex
 local darcula_solid = require("lush_theme.darcula-solid")
 local spec = lush.extends({ darcula_solid }).with(function()
     -- Your modifications go here...
     -- Pallete copied from https://github.com/briones-gabriel/darcula-solid.nvim/blob/main/lua/lush_theme/darcula-solid.lu
 
-    local bg = darcula_solid.Normal.bg
-    local red     = hsl(1, 77, 59)
-    local yellow  = hsl(37, 100, 71)
-    local green   = hsl(83, 27, 53)
+    local bg = hsl(210, 7, 16)
+    local red = hsl(1, 77, 59)
+    local yellow = hsl(37, 100, 71)
+    local green = hsl(83, 27, 53)
 
     return {
+        Normal({ bg = bg, fg = darcula_solid.Normal.fg }),
         GitSignsAdd({ fg = green.da(20) }),
         GitSignsChange({ fg = yellow.da(20) }),
         GitSignsDelete({ fg = red }),
