c2845e51215e566cb1c16fcfd0caea0d70e47091
edward-apollo <edward@apolloresearch.ai>
2024-03-13 18:56:38 +0000

    move pycache files

--- python/python.zsh
+++ python/python.zsh
@@ -1,3 +1,6 @@
+# Move pycache files
+export PYTHONPYCACHEPREFIX=/tmp/pycache
+
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
   if command pyenv virtualenv --help 1>/dev/null 2>&1; then
