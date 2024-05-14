8febd93e5b3472b7b077282ce195845f9b07135d
Edward Rees <ed@anthropic.com>
2024-05-14 21:51:01 +0100

    add minimall install-min.sh

--- install-min.sh
+++ install-min.sh
@@ -1,15 +1,22 @@
-PLUGINS_DIR=${ZSH_PLUGIN_DIR:-"$HOME/.zsh/plugins"}
+export OH_MY_ZSH=$HOME/.oh-my-zsh
 
 brew install coreutils
 brew install fzf
 brew install bat
 brew install git-delta
 
+rm -f $OH_MY_ZSH/.zshrc.pre-oh-my-zsh  # I don't want t a backup
+ZSH_CUSTOM=$OH_MY_ZSH/custom
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+rm -rf $ZSH_CUSTOM
+
 git clone https://github.com/romkatv/powerlevel10k.git \
-    ${PLUGINS_DIR}/powerlevel10k 
+    ${ZSH_CUSTOM}/themes/powerlevel10k 
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
-    ${PLUGINS_DIR}/zsh-syntax-highlighting 
+    ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting 
 
 git clone https://github.com/zsh-users/zsh-autosuggestions \
-    ${PLUGINS_DIR}/zsh-autosuggestions 
+    ${ZSH_CUSTOM}/plugins/zsh-autosuggestions 
