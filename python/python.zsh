a86c1ab35a859aca89ebc7f3da7b479491a8cb5c
Edward Rees <ed@anthropic.com>
2024-06-01 12:33:37 -0700

    python module fuzzy find binding

--- python/python.zsh
+++ python/python.zsh
@@ -1,6 +1,19 @@
 # Move pycache files
 export PYTHONPYCACHEPREFIX=/tmp/pycache
 
+function fzf-run-python-m {
+    {
+        local _path=$(find . -name \*.py | fzf | rev | cut -d. -f2- | rev | cut -d/ -f3- | tr '/' '.')
+        if [ ! -z "$_path" ]
+        then
+            BUFFER="python -m $_path "
+            zle end-of-line
+        fi
+    }
+}
+zle -N fzf-run-python-m
+bindkey '\ep' fzf-run-python-m
+
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
   if command pyenv virtualenv --help 1>/dev/null 2>&1; then
