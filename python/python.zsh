4a76af8b35cb54c3dec413c3020b5d01c8f1a414
Edward Rees <ed@anthropic.com>
2024-07-18 18:39:30 +0100

    python keybindings

--- python/python.zsh
+++ python/python.zsh
@@ -1,6 +1,18 @@
 # Move pycache files
 export PYTHONPYCACHEPREFIX=/tmp/pycache
 
+# convert a python command to a debug command
+function replace-python {
+  if [[ $BUFFER =~ ^python\ .* ]]; then
+    BUFFER="python3 -m debugpy --listen 5678 --wait-for-client ${BUFFER#python }"
+    CURSOR+=51
+    zle reset-prompt
+  fi
+}
+zle -N replace-python
+bindkey "\ed" replace-python
+
+
 function fzf-run-python-m {
     {
         local _path=$(find . -name \*.py | fzf | rev | cut -d. -f2- | rev | cut -d/ -f3- | tr '/' '.')
@@ -8,6 +20,7 @@ function fzf-run-python-m {
         then
             BUFFER="python -m $_path "
             zle end-of-line
+            zle reset-prompt
         fi
     }
 }
