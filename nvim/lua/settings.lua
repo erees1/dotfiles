f544db45c135a0094e1b0e4355d0902fc37d68c4
Edward Rees <ed@anthropic.com>
2025-08-05 13:21:01 +0100

    Use new vim.hl api instead of vim.highlight

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -51,11 +51,11 @@ vim.o.fillchars = "diff:╱"
 vim.api.nvim_create_augroup("Misc", { clear = true })
 
 -- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
+-- See `:help vim.hl.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.on_yank()
   end,
   group = highlight_group,
   pattern = '*',
