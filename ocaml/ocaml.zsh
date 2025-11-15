30041d828a6bf2dccdde00c8866b2beaf0745e19
Edward Rees <ed@anthropic.com>
2025-11-15 19:37:57 +0000

    Trim down

--- ocaml/ocaml.zsh
+++
@@ -1,2 +0,0 @@
-# opam configuration
-[[ ! -r /Users/edwardr/.opam/opam-init/init.zsh ]] || source /Users/edwardr/.opam/opam-init/init.zsh  > /dev/null 2> /dev/null
--- ruby/manual_install.sh
+++
@@ -1,7 +0,0 @@
-if [ "$(uname -s)" == "Darwin" ]
-then
-  brew install rbenv
-  eval "$(rbenv init -)"
-  rbenv install 2.7.2
-  rbenv global 2.7.2
-fi
